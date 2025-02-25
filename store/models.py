aafd36d7e98a7c07abd2c80c98bb45514bf26c85
Issam El Maqrini <elmaqrini@hotmail.com>
2025-02-26 00:01:18 +0100

    Initial commit

---
+++ store/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    quantity = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity})"
+
+
+
